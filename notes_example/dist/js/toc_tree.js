c309d9de1e2fec761a721f98389492a810b95207
Vasco Ferreira <vasco_mmf@hotmail.com>
2021-02-17 00:31:36 +0000

    Updated the example notes folder

---
+++ notes_example/dist/js/toc_tree.js
@@ -0,0 +1,104 @@
+const tocTree = [
+	{
+		"name": "Ox-Tailwind Export Back-End", 
+		"index": "0", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Light and Dark Themes", 
+		"index": "1", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Known bugs", 
+		"index": "2", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Installation", 
+		"index": "3", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Spacemacs", 
+		"index": "4", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Doom Emacs", 
+		"index": "5", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Your own config", 
+		"index": "6", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Notes structure", 
+		"index": "7", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Publish settings", 
+		"index": "8", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Notes output directory", 
+		"index": "9", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Elements", 
+		"index": "10", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Markup", 
+		"index": "11", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Formulas", 
+		"index": "12", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Blocks", 
+		"index": "13", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Custom Blocks", 
+		"index": "14", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Customization", 
+		"index": "15", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Dark Theme", 
+		"index": "16", 
+		"parent": "Ox-Tailwind Export Back-End", 
+		"file": "./index.html"
+	},
+];
